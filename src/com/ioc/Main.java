046cbfbc6013e1fedbdbf3b81f21327073629b66
sk10salman <salman9694262768@gmail.com>
2021-08-08 16:34:58 +0530

    Modified main.java

--- src/com/ioc/Main.java
+++ src/com/ioc/Main.java
@@ -1,13 +1,10 @@
 package com.ioc;
-
-
-
 public class Main {
     public static void main(String[] args)
     {
     	Engine eng= new Car();
     	eng.start();
-    	
+    	System.out.print("hello World");
     	
     }
 }
